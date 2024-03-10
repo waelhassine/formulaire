ac2a7a07a5ad475c5430f662fbf93405657d945c
Wael hassine <wael.hassine0@gmail.com>
2024-03-10 23:52:58 +0100

    test send email

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,7 @@
 import sendEmail from '@/lib/send_email';
 import { NextRequest } from 'next/server';
 import { PDFDocument, rgb, PDFPage } from 'pdf-lib';
+import nodemailer from 'nodemailer';
 
 interface KeyMapping {
   [key: string]: string;
@@ -135,9 +136,39 @@ export async function POST(request: NextRequest) {
   drawData(data, page, x, y);
 
   const pdfBytes = await pdfDoc.save();
+  const transporter = nodemailer.createTransport({
+    host: 'ssl0.ovh.net', // OVH SMTP server
+    port: 587, // SMTP port (could be different based on your settings, e.g., 465)
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'noreply@sea-electronics.com', // Your OVH email address
+      pass: 'wassimSEA2023', // Your OVH email password
+    },
+  });
 
-  // Example: Uncomment and use this line if you intend to actually send the email
-  await sendEmail(pdfBytes);
+  const mailOptions = {
+    from: '"Sender Name" <no-replay@sea-electronics.com>',
+    to: 'wael.hassine0@gmail.com',
+    subject: 'Here is your PDF',
+    text: 'Please find the attached PDF.',
+    attachments: [
+      {
+        filename: 'generated-pdf.pdf',
+        content: Buffer.from(pdfBytes), // Convert Uint8Array to Buffer
+        contentType: 'application/pdf',
+      },
+    ],
+  };
+
+  await transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  // // Example: Uncomment and use this line if you intend to actually send the email
+  // await sendEmail(pdfBytes);
 
   return new Response(pdfBytes, {
     status: 200,
