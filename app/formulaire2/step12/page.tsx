f761aa8b23e9054e3ce4cd41d3b904c79ca19bcd
Wael hassine <wael.hassine0@gmail.com>
2024-03-13 23:04:56 +0100

    Fix issue 3

--- app/formulaire2/step12/page.tsx
+++ app/formulaire2/step12/page.tsx
@@ -2,12 +2,12 @@
 import useAppFormContext from '@/lib/hooks/useAppFormContext2';
 import { useRouter } from 'next/navigation';
 import ProgressHeader from '@/components/ui/progressHeader';
-import { Button } from "@/components/ui/button"
-import { PlusCircledIcon } from "@radix-ui/react-icons";
+import { Button } from '@/components/ui/button';
+import { PlusCircledIcon } from '@radix-ui/react-icons';
 import Cardstep12 from './Cardstep12';
 import { useFieldArray } from 'react-hook-form';
 import FormActions from '@/components/FormActions';
-import { CardInfo } from '@/types/form';
+import { CardInfo } from '@/types/form2';
 
 export default function FormulaireStep8() {
   const router = useRouter();
@@ -21,20 +21,23 @@ export default function FormulaireStep8() {
     control,
     name: 'card_conducteur', // This name should match the one in your form's initial state or schema
   });
-  
+
   const validateStep = async () => {
     const result = await trigger();
-    console.log('data' , getValues());
+    console.log('data', getValues());
 
     if (result && isValid) {
       router.push('/formulaire2/step13');
     }
   };
-    
-    return (
-        <div className="w-full">
-        <ProgressHeader val={90} />
-        <button className="flex flex-row space-x-2 items-center justify-center mt-6" onClick={() => router.push('step11')}>
+
+  return (
+    <div className="w-full">
+      <ProgressHeader val={90} />
+      <button
+        className="flex flex-row space-x-2 items-center justify-center mt-6"
+        onClick={() => router.push('step11')}
+      >
         <svg width="25" height="25" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
@@ -46,24 +49,22 @@ export default function FormulaireStep8() {
         <p className="text-lg">Précédent</p>
       </button>
 
-        <div className="flex flex-col space-y-4 w-2/3">
-          <p className="flex flex-row text-2xl pt-12">
-          Conducteur 
- <span className="text-red-700 px-1">secondaire</span>
-          </p>
-          <p className="flex flex-row text-2xl pt-12">
-          Antécédents dassurance
-          </p>
+      <div className="flex flex-col space-y-4 w-2/3">
+        <p className="flex flex-row text-2xl pt-12">
+          Conducteur
+          <span className="text-red-700 px-1">secondaire</span>
+        </p>
+        <p className="flex flex-row text-2xl pt-12">Antécédents dassurance</p>
 
-          {fields.map((field, index) => (
+        {fields.map((field, index) => (
           <Cardstep12
             key={field.id} // React Hook Form uses 'id' for key management in field arrays
             index={index}
             onClose={() => remove(index)}
           />
         ))}
-          
-          <Button type="button" variant="secondary" onClick={() => append({} as CardInfo)}>
+
+        <Button type="button" variant="secondary" onClick={() => append({} as CardInfo)}>
           <PlusCircledIcon className="mr-2 h-4 w-4" /> Ajouter un antécédent
         </Button>
         <FormActions>
@@ -71,8 +72,7 @@ export default function FormulaireStep8() {
             Suivant
           </Button>
         </FormActions>
-        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
